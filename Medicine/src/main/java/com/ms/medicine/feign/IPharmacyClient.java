85a7a648f5c45a18ef9486e9c48d2259831856bc
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2024-07-18 19:43:36 +0100

    adding docker

--- Medicine/src/main/java/com/ms/medicine/feign/IPharmacyClient.java
+++ Medicine/src/main/java/com/ms/medicine/feign/IPharmacyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "PHARMACY-SERVICE/pharmacy", url = "http://localhost:8080/pharmacy")
+@FeignClient(name = "PHARMACY-SERVICE/pharmacy")
 public interface IPharmacyClient {
     @GetMapping("/{id}")
     PharmacyDto getPharmacyById(@PathVariable("id") String id);
