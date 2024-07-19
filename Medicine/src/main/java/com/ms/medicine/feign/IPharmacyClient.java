d26e366858ed7a0a46a3734694f7f59032ac35db
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2024-07-19 16:55:37 +0100

    add config server

--- Medicine/src/main/java/com/ms/medicine/feign/IPharmacyClient.java
+++ Medicine/src/main/java/com/ms/medicine/feign/IPharmacyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "PHARMACY-SERVICE/pharmacy")
+@FeignClient(name = "PHARMACYSERVICE/pharmacy")
 public interface IPharmacyClient {
     @GetMapping("/{id}")
     PharmacyDto getPharmacyById(@PathVariable("id") String id);
